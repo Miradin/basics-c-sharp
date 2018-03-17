0b2bc41f99f8716a5001db64239aa5462b2bd785
Miradin <alexandr0202@gmail.com>
2018-03-17 12:56:33 +0100

    change according to HW

--- TurtleTraining.cs
+++ TurtleTraining.cs
@@ -5,22 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.SmallBasic.Library;
 
+
+
 namespace TurtleTraining
 {
-	class Program
-	{
-		static void Main( string[] args )
-		{
-			Turtle.Speed = 9;
-			int i = 0;
-			while (i < 4)
-			{
-				Turtle.Move( 100 );
-				//Turtle.TurnRight();
-				//Turtle.Turn( 90 );
-				Turtle.TurnLeft();
-				i++;
-			}
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Turtle.Speed = 9;
+            int i = 0;
+            int Stepsize = 20;
+            while (i < 10)
+            {
+                if (i%2 == 0)
+                {
+                    Turtle.Move(Stepsize);
+                    Turtle.TurnRight();
+                    Turtle.Move(Stepsize);
+                    Turtle.TurnRight();
+                } else
+                {
+                    Turtle.Move(Stepsize);
+                    Turtle.TurnLeft();
+                    Turtle.Move(Stepsize);
+                    Turtle.TurnLeft();
+                }
+                
+                i++;
+            }
+
+        }
+    }
 }
